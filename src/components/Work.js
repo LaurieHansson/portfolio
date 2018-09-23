2aece40123ca0fb2ded71f1e9ab928cd52567bcc
LHansson <lhanssonatx@gmail.com>
2018-09-23 10:19:34 -0500

    corrected Work section

--- src/components/Work.js
+++ src/components/Work.js
@@ -16,16 +16,16 @@ const Work = (props) => {
               title="LOCALHOSTERS"
               desc="LOCALHOSTERS is a websocket application I built to develop a better understanding 
               of severside functionality and broadcasting messages through servers."
-              skills=" HTML, CSS (bootstrap, CSS-grid), Javascript (Jquery)"
+              skills=" HTML, CSS, Javascript, Socket.io "
               type="passion"
             />
           </Col>
           <Col xs={12} md={4} lg={6}>
             <Project className="project"
               title="The Hustle Assistant"
-              desc="LOCALHOSTERS is a websocket application I built to develop a better understanding 
-              of severside functionality and broadcasting messages through servers."
-              skills=" HTML, CSS (bootstrap, CSS-grid), Javascript (Jquery)"
+              desc="The Hustle Assistant is a react to-do list built for the minimalist on the rise.
+              The clean, distraction-free UI ensures endless productivity."
+              skills=" React, CSS"
               type="passion"
             />
           </Col>
